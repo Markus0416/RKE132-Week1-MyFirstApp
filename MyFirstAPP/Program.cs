7f327fd0736de20812e13d8e4433667e888a4c81
Markus <markus.mursal@tktk.ee>
2024-03-07 12:29:53 +0200

    first commit

---
+++ MyFirstAPP/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is my a commet to my code 
+
+//rakendus küsib kasutajal sisestada teme nime 
+//rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter yout name: ");
+
+//string - sõne 
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !");
